6d7b9d479b67842b99a2e34f55c532c92d9976ef
JL <woyoureqing@gmail.com>
2026-01-26 17:13:49 +0800

    添加刮板输送机的双方向遍历

--- Assets/MyFront.cs
+++ Assets/MyFront.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MyFront : MonoBehaviour {
 
+    private void Awake() {
+        if (transform.hasChanged) {
+            transform.hasChanged = false;
+        }
+    }
     //void Update() {
     //    if (transform.hasChanged) {
     //        transform.hasChanged = false;
--- Assets/MyManager.cs
+++ Assets/MyManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 public class MyManager : MonoBehaviour {
@@ -46,49 +48,33 @@ public class MyManager : MonoBehaviour {
     }
 
     private void Update() {
-
-        //for (int i = 0; i < zhijias.Length; i++) {
-        //    MyZhijia curZhijia = zhijias[i];
-        //    curZhijia.front.transform.localPosition += new Vector3(-moveSpeed * Time.deltaTime * (1), 0f, 0f);
-        //}
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            // 处理邻居刮板和推移连杆
-            //Debug.Log("处理邻居刮板和推移连杆");
-            int index = 0;
-            while (index < zhijias.Length) {
-                if (!zhijias[index].isActive) {
-                    zhijias[index].Follow();
-                }
-                index++;
-            }
-        }
-
         // 存在异常角点时
-        while (ExistAbnormalCornerPoints()) {
-            // 1.尝试旋转中部槽自身（保证中部槽角度在0~3°内，且每个中部槽左侧一角点与右侧一角点在同一个位置）
-            // 2.若中部槽旋转仍有异常点，则推移连杆向前移动Δd距离(需保证已经到最大行程的推移连杆不动，S弯区段的推移连杆才能动)
+        //while (ExistAbnormalCornerPoints()) {
+        //    // 1.尝试旋转中部槽自身（保证中部槽角度在0~3°内，且每个中部槽左侧一角点与右侧一角点在同一个位置）
+        //    // 2.若中部槽旋转仍有异常点，则推移连杆向前移动Δd距离(需保证已经到最大行程的推移连杆不动，S弯区段的推移连杆才能动)
 
 
-        }
+        //}
 
         if (zhijias.Any(t => t.front.transform.hasChanged)) {
-            int index = 0;
-            while (index < zhijias.Length) {
-                if (!zhijias[index].isActive) {
-                    zhijias[index].Follow();
+            int startIndex = Array.FindIndex(zhijias, t => t.isActive);
+            zhijias[startIndex].front.transform.hasChanged = false;
+            int maxOffset = Math.Max(startIndex, zhijias.Length - 1 - startIndex);
+
+            for (int offset = 1; offset <= maxOffset; offset++) {
+                int rightIndex = startIndex + offset;
+                if (rightIndex < zhijias.Length) {
+                    zhijias[rightIndex].Follow();
+                    zhijias[rightIndex].front.transform.hasChanged = false;
                 }
-                zhijias[index].front.transform.hasChanged = false;
-                index++;
+
+                int leftIndex = startIndex - offset;
+                if (leftIndex >= 0) {
+                    zhijias[leftIndex].Follow();
+                    zhijias[leftIndex].front.transform.hasChanged = false;
+                }
             }
         }
-        //int index = 0;
-        //while (index < zhijias.Length) {
-        //    if (!zhijias[index].isActive) {
-        //        zhijias[index].TryToModify();
-        //    }
-        //    index++;
-        //}
 
     }
 
